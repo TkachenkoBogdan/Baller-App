99391a1c9d7b58b7b97b587fac12a3ef789482fb
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-08-16 07:36:35 +0300

    Outlets have been hooked up on the main VC.

--- Baller/ViewController.swift
+++ Baller/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: - Outlets:
+    @IBOutlet weak var ballImageView: UIImageView!
+    @IBOutlet weak var questionAnswerLabel: UILabel!
+    
+    
+    // MARK: - Lifecycle:
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
