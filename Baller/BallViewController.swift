3fa3fd5dddf111974e1cc2b65d73af51c73a8962
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-08-23 16:41:41 +0300

    BallViewController added

---
+++ Baller/BallViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Baller
+//
+//  Created by Богдан Ткаченко on 8/16/19.
+//  Copyright © 2019 Богдан Ткаченко. All rights reserved.
+//
+
+import UIKit
+
+class BallViewController: UIViewController {
+    
+    private let answerProvider: AnswerProviding = AnswerProvider()
+    
+    // MARK: - Outlets:
+    @IBOutlet private weak var ballImageView: UIImageView!
+    @IBOutlet private weak var answerLabel: UILabel!
+    @IBOutlet private weak var statusLabel: UILabel!
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        self.ballImageView.shake()
+        self.setLabelsVisibility(to: true)
+        
+        answerProvider.getAnswer { (result) in
+            switch result {
+            case .success(let answer):
+                self.updeteAnswerLabel(with: answer.title)
+            case .failure:
+                self.updeteAnswerLabel(with: "Oh, snap! Try again")
+            }
+        }
+    }
+}
+
+// MARK: - Helpers:
+extension BallViewController {
+    
+    private func updeteAnswerLabel(with answer: String) {
+        DispatchQueue.main.async {
+            self.setLabelsVisibility(to: false)
+            self.statusLabel.alpha = 0
+            self.answerLabel.alpha = 1
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                UIView.animate(withDuration: 1, animations: {
+                    self.statusLabel.alpha = 1
+                    self.answerLabel.alpha = 0
+                })
+            })
+            self.answerLabel.fadeTransition(withDuration: 1)
+            self.answerLabel.text = answer
+        }
+    }
+    
+    private func setLabelsVisibility(to isHidden: Bool) {
+        self.answerLabel.isHidden = isHidden
+        self.statusLabel.isHidden = isHidden
+    }
+}
