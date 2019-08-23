4980a45b2220654e068f1001362f5cc1611b69e7
Bogdan Tkachenko <emptypagebluesky@gmail.com>
2019-08-23 16:13:27 +0300

    concrete OnlineProvider added

--- Baller/AnswerProvider.swift
+++ Baller/AnswerProvider.swift
@@ -14,8 +14,11 @@ protocol AnswerProviding {
 
 class AnswerProvider: AnswerProviding {
     
-    private let offlineProvider = OfflineProvider()
-    private let onlineProvider = OnlineProvider()
+  
+ 
+    
+     private let onlineProvider = OnlineProvider()
+     private let offlineProvider = OfflineProvider()
     
     func getAnswer(completionHandler: @escaping (Result<Answer, Error>) -> Void) {
         onlineProvider.getAnswer { (result) in
---
+++ Baller/OnlineProvider.swift
@@ -0,0 +1,38 @@
+//
+//  OnlineAnswerService.swift
+//  Baller
+//
+//  Created by Богдан Ткаченко on 8/16/19.
+//  Copyright © 2019 Богдан Ткаченко. All rights reserved.
+//
+
+import Foundation
+
+struct OnlineProvider: AnswerProviding {
+    
+    private struct Endpoint {
+        let host: String
+        let path: String
+        
+        var url: URL? {
+            return URL(string: host + path)
+        }
+    }
+    
+    private let endpoint = Endpoint(host: "https://8ball.delegator.com",
+                                    path: "/magic/JSON/whatislove?")
+    
+    func getAnswer(completionHandler: @escaping (Result<Answer, Error>) -> Void) {
+        guard let url = endpoint.url else { return }
+        let request = URLRequest(url: url, timeoutInterval: 2.5)
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let data = data, error == nil,
+                let answer = try? JSONDecoder().decode(Answer.self, from: data) {
+                completionHandler(Result.success(answer))
+            } else {
+                completionHandler(Result.failure(error!))
+            }
+        }.resume()
+    }
+}
